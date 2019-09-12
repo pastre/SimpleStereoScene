a5bae9886cc6e1238a4f0eaee2619b5a502f6d51
pastre <pastre68@gmail.com>
2019-09-12 15:40:56 -0300

    Initial Commit

---
+++ SimpleStereoScene/SimpleStereoScene.h
@@ -0,0 +1,19 @@
+//
+//  SimpleStereoScene.h
+//  SimpleStereoScene
+//
+//  Created by Bruno Pastre on 12/09/19.
+//  Copyright © 2019 Bruno Pastre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleStereoScene.
+FOUNDATION_EXPORT double SimpleStereoSceneVersionNumber;
+
+//! Project version string for SimpleStereoScene.
+FOUNDATION_EXPORT const unsigned char SimpleStereoSceneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleStereoScene/PublicHeader.h>
+
+
